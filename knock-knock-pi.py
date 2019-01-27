84f63cda324383928a9b74b56ac725da00a43986
Chirag Jariwala <sc.cjhackerz@yahoo.com>
2019-01-27 19:06:01 +0530

    Feature update
    added support for email and pi camera, removed twitter client.

--- knock-knock-pi.py
+++ knock-knock-pi.py
@@ -1,35 +1,35 @@
 import RPi.GPIO as GPIO
+from picamera import PiCamera
+import requests
+import os
 import time
-import tweepy
 
 #Raspberry pi GPIO config. according to wiring setup
 GPIO.setmode(GPIO.BCM)
 TRIG = 23
 ECHO = 24
+camera = PiCamera()
 
-# This is twitter API configuiration from https://apps.twitter.com/
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACESS_KEY = ""
-ACCESS_SECRET = ""
+def sendMail(sendfile):
+	return requests.post(
+		"https://api.mailgun.net/v3/YOUR_DOMAIN/messages",
+		auth=("api", os.environ['MAILGUN_API_KEY']),
+		files=[("attachment", open(sendfile, 'rb'))],
+		data={
+		"from": "HomeSecurity Bot <bot@YOUR_DOMAIN>",
+		"to": ["Full Name", "alice@example.com"],
+		"subject": "Security Alert",
+		"text": "Someone is at front door!"
+		})
 
-#Verifying auth.
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-
-tweet = tweepy.API(auth) #API consumed into user defined object
-
-tweet.update_status('This is test!') #Test status post on twitter timeline
-
-#HC-SR04 script
 while True:		#Continues code execution
-	print "Distance Measurement In Progress"
+	print("Distance Measurement In Progress")
 
 	GPIO.setup(TRIG,GPIO.OUT)
 	GPIO.setup(ECHO,GPIO.IN)
 
 	GPIO.output(TRIG, False)
-	print "Waiting For Sensor To Settle"
+	print("Waiting For Sensor To Settle")
 	time.sleep(2)
 
 	GPIO.output(TRIG, True)
@@ -48,18 +48,22 @@ while True:		#Continues code execution
 
 	distance = round(distance, 2)
 
-	print "Distance:",distance,"cm"
+	print("Distance: {} cm".format(distance))
 
 	if distance < 20: #Make tweet if somthing found at less than 20cm range
-		tweet.update_status('Knock, Knock! Someone is at your Home!')
-		print "Tweet posted!"
-
-	GPIO.cleanup()
-
-
-
-
-
+		print("Found something in close range! Recording 10 second footage...")
+		footage_out_fname = os.getcwd() + "/" + "security_footage-" + str(time.time()) + '.h264'
+		camera.start_recording(footage_out_fname)
+		time.sleep(5)
+		camera.stop_recording()
+		time.sleep(1)
+		print("Converting file to .mp4")
+		os.system("ffmpeg -framerate 30 -i {} -c copy security_footage.mp4".format(footage_out_fname))
+		time.sleep(2)
+		print("Sending email notification...")
+		print(sendMail(os.getcwd() + "/security_footage.mp4"))
+		os.system("rm -drf security_footage.mp4")
+GPIO.cleanup()
 
 # =>Code written by: CJHackerz
 # ++ https://github.com/cjhackerz
